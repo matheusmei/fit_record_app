25815b81347141bf48236d55fc50ed6d4e871d5b
matheusmei <71150878+matheusmei@users.noreply.github.com>
2022-05-04 15:44:27 -0300

    initial commit

---
+++ lib/widgets/componation/colors_app.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class ColorsApp{
+static Color maincolor1 = const Color(0xff1F306E);
+static Color maincolor2 = const Color(0xff8F3B76);
+static Color maincolor3 = const Color(0xffF5487F);
+static Color maincolor4 = const Color(0xffE5E5E5);
+static Color maincolor5 = const Color(0xffC7417B);
+}
---
+++ lib/widgets/componation/font_app.dart
@@ -0,0 +1,16 @@
+import 'package:fit_record_app/widgets/componation/colors_app.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class FontApp{
+static TextStyle logotitle = GoogleFonts.boogaloo(fontSize: 50, color: ColorsApp.maincolor4);
+static TextStyle mainfont16 = GoogleFonts.openSans(fontSize: 16);
+static TextStyle mainfont18 = GoogleFonts.openSans(fontSize: 18);
+static TextStyle mainfont20 = GoogleFonts.openSans(fontSize: 20);
+static TextStyle mainfont30 = GoogleFonts.openSans(fontSize: 30);
+static TextStyle mainfont32 = GoogleFonts.openSans(fontSize: 32);
+static TextStyle mainfont36 = GoogleFonts.openSans(fontSize: 36);
+static TextStyle mainfont42 = GoogleFonts.openSans(fontSize: 42);
+static TextStyle mainfont48 = GoogleFonts.openSans(fontSize: 48);
+static TextStyle mainfont45 = GoogleFonts.openSans(fontSize: 45);
+}
