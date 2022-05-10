e9f61d09695804299240d2e832c981b68ae1bad8
matheusmei <71150878+matheusmei@users.noreply.github.com>
2022-05-10 13:03:59 -0300

    create firebase

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Fit Record',
-      home: MusculaGroupCollection(),
+      home: LoginPage(),
 
     );
   }
